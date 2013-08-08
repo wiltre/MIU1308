b66b0499c396e3cd65c4b471881627e0cbff2c09
Wiltre Santiago <wiltres@gmail.com>
2013-08-07 23:33:44 -0400

    adding my VFW fully working CRUD

---
+++ week1/js/json.js
@@ -0,0 +1,55 @@
+/**
+ * @author Wil
+ * Title: json data for the item app
+ * By: Wiltre Santiago 
+ * For: VFW Project 4
+ * This is the js for index.html
+ */
+
+var json={
+	"store1":{
+		"category"	: ["Category: ", "Clothing"],
+		"fname"	  	: ["Fruit Name: ", "CK"],
+		"sweb"	  	: ["Store Website: ", "www.ck.com"],
+		"sLocation"	: ["Store Location: ", "National"],
+		"npurchase"	: ["Near Purchase: ", "Yes"],
+		"priority"	: ["Item Priority: ", "5"],
+		"date"		: ["Date: ", "11/12/13"],
+		"notes"		: ["Product Notes: ", "Great Company"]
+		
+	},
+	"store2":{
+		"category"	: ["Category: ", "Electronics"],
+		"fname"	  	: ["Store Name: ", "Best Buy"],
+		"sweb"	  	: ["Store Website: ", "www.bestbuy.com"],
+		"sLocation"	: ["Store Location: ", "National"],
+		"npurchase"	: ["Near Purchase: ", "Yes"],
+		"priority"	: ["Item Priority: ", "5"],
+		"date"		: ["Date: ", "10/12/13"],
+		"notes"		: ["Product Notes: ", "Some good prices"]
+		
+	},
+	"store3":{
+		"category"	: ["Category: ", "Transport"],
+		"fname"	  	: ["Store Name: ", "Ferrari"],
+		"sweb"	  	: ["Store Website: ", "www.ferrari.com"],
+		"sLocation"	: ["Store Location: ", "National"],
+		"npurchase"	: ["Near Purchase: ", "Yes"],
+		"priority"	: ["Item Priority: ", "9"],
+		"date"		: ["Date: ", "10/12/13"],
+		"notes"		: ["Product Notes: ", "My next car "]
+		
+	},
+	"store4":{
+		"category"	: ["Category: ", "Jewerly"],
+		"fname"	  	: ["Store Name: ", "Zales"],
+		"sweb"	  	: ["Store Website: ", "www.zales.com"],
+		"sLocation"	: ["Store Location: ", "International"],
+		"npurchase"	: ["Near Purchase: ", "Yes"],
+		"priority"	: ["Item Priority: ", "10"],
+		"date"		: ["Date: ", "07/12/13"],
+		"notes"		: ["Product Notes: ", "My wife wedding ring"]
+		
+	}
+	
+}
