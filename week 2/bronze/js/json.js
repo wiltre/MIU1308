de9bb88e47e062237b9e3172fd572692da924868
Wiltre Santiago <wiltres@gmail.com>
2013-08-11 19:01:03 -0400

    bronze and gold folders

---
+++ week 2/bronze/js/json.js
@@ -0,0 +1,231 @@
+/**
+ * @author Wil
+ * Title: json data for the item app
+ * By: Wiltre Santiago 
+ * For: MIU 1308 Project 1
+ * This is the json file for the default data 
+ */
+
+var json={
+	"store1":{
+		"category"	: ["Item Category: ", "Berries"],
+		"fname"	  	: ["Fruit Name: ", "Rassberry"],
+		"sweb"	  	: ["Store Name: ", "ShopRIte"],
+		"sLocation"	: ["Require Refrigeration ", "Yes"],
+		"npurchase"	: ["Favorite: ", "Yes"],
+		"priority"	: ["Days that last: ", "6"],
+		"date"		: ["Date: ", "11/12/13"],
+		"notes"		: ["Product Notes: ", "Taste Greate with honey"]
+		
+	},
+	"store2":{
+		"category"	: ["Item Category: ", "Citrus"],
+		"fname"	  	: ["Fruit Name: ", "Orange"],
+		"sweb"	  	: ["Store Name: ", "Costco"],
+		"sLocation"	: ["Require Refrigeration ", "NO"],
+		"npurchase"	: ["Favorite: ", "Yes"],
+		"priority"	: ["Days that last: ", "8"],
+		"date"		: ["Date: ", "11/15/13"],
+		"notes"		: ["Product Notes: ", "Make good Juice"]
+		
+	},
+	"store3":{
+		"category"	: ["Item Category: ", "Grapes"],
+		"fname"	  	: ["Fruit Name: ", "WhiteGrapes"],
+		"sweb"	  	: ["Store Name: ", "StopNshop"],
+		"sLocation"	: ["Require Refrigeration ", "Yes"],
+		"npurchase"	: ["Favorite: ", "NO"],
+		"priority"	: ["Days that last: ", "9"],
+		"date"		: ["Date: ", "10/12/13"],
+		"notes"		: ["Product Notes: ", "Nice for Munchies"]
+		
+	},
+	"store4":{
+			"category"	: ["Item Category: ", "Tropical"],
+		"fname"	  	: ["Fruit Name: ", "Bananna"],
+		"sweb"	  	: ["Store Name: ", "ShopRite"],
+		"sLocation"	: ["Require Refrigeration ", "No"],
+		"npurchase"	: ["Favorite: ", "Yes"],
+		"priority"	: ["Days that last: ", "4"],
+		"date"		: ["Date: ", "11/19/13"],
+		"notes"		: ["Product Notes: ", "Great for snacks and smothies"]
+		
+	},
+	"store5":{
+			"category"	: ["Item Category: ", "Other"],
+		"fname"	  	: ["Fruit Name: ", "Avocado"],
+		"sweb"	  	: ["Store Name: ", "Costco"],
+		"sLocation"	: ["Require Refrigeration ", "NO"],
+		"npurchase"	: ["Favorite: ", "No"],
+		"priority"	: ["Days that last: ", "6"],
+		"date"		: ["Date: ", "9/12/13"],
+		"notes"		: ["Product Notes: ", "Awesome for side dish"]
+		
+	},
+	"store6":{
+			"category"	: ["Item Category: ", "Berries"],
+		"fname"	  	: ["Fruit Name: ", "Strawberry"],
+		"sweb"	  	: ["Store Name: ", "Wegmans"],
+		"sLocation"	: ["Require Refrigeration ", "Yes"],
+		"npurchase"	: ["Favorite: ", "Yes"],
+		"priority"	: ["Days that last: ", "7"],
+		"date"		: ["Date: ", "11/01/13"],
+		"notes"		: ["Product Notes: ", "Can be combine with Banana for smoothie"]
+		
+	},
+	"store7":{
+			"category"	: ["Item Category: ", "Citrus"],
+		"fname"	  	: ["Fruit Name: ", "Lemon"],
+		"sweb"	  	: ["Store Name: ", "ShopRIte"],
+		"sLocation"	: ["Require Refrigeration ", "no"],
+		"npurchase"	: ["Favorite: ", "No"],
+		"priority"	: ["Days that last: ", "6"],
+		"date"		: ["Date: ", "11/4/13"],
+		"notes"		: ["Product Notes: ", "Great for lemonade"]
+		
+	},
+	"store8":{
+			"category"	: ["Item Category: ", "Grapes"],
+		"fname"	  	: ["Fruit Name: ", "Reg Grapes"],
+		"sweb"	  	: ["Store Name: ", "Sam's"],
+		"sLocation"	: ["Require Refrigeration ", "Yes"],
+		"npurchase"	: ["Favorite: ", "Yes"],
+		"priority"	: ["Days that last: ", "8"],
+		"date"		: ["Date: ", "11/16/13"],
+		"notes"		: ["Product Notes: ", "Nice combination with other fruits"]
+		
+	},
+	"store9":{
+			"category"	: ["Item Category: ", "Tropical"],
+		"fname"	  	: ["Fruit Name: ", "Coconut"],
+		"sweb"	  	: ["Store Name: ", "ShopRIte"],
+		"sLocation"	: ["Require Refrigeration ", "No"],
+		"npurchase"	: ["Favorite: ", "Yes"],
+		"priority"	: ["Days that last: ", "10"],
+		"date"		: ["Date: ", "11/12/13"],
+		"notes"		: ["Product Notes: ", "Nice for Piña Colada"]
+		
+	},
+	"store10":{
+			"category"	: ["Item Category: ", "Other"],
+		"fname"	  	: ["Fruit Name: ", "Pasion Fruit"],
+		"sweb"	  	: ["Store Name: ", "Compare"],
+		"sLocation"	: ["Require Refrigeration ", "No"],
+		"npurchase"	: ["Favorite: ", "Yes"],
+		"priority"	: ["Days that last: ", "10"],
+		"date"		: ["Date: ", "11/20/13"],
+		"notes"		: ["Product Notes: ", "Awesome for Juice require some sugar"]
+		
+	},
+	"store11":{
+			"category"	: ["Item Category: ", "Other"],
+		"fname"	  	: ["Fruit Name: ", "Guanabana"],
+		"sweb"	  	: ["Store Name: ", "Compare"],
+		"sLocation"	: ["Require Refrigeration ", "No"],
+		"npurchase"	: ["Favorite: ", "Yes"],
+		"priority"	: ["Days that last: ", "10"],
+		"date"		: ["Date: ", "11/20/13"],
+		"notes"		: ["Product Notes: ", "Great for juice and smoothie sweet fruit"]
+		
+	},
+	"store12":{
+		"category"	: ["Item Category: ", "Citrus"],
+		"fname"	  	: ["Fruit Name: ", "Grape Fruit"],
+		"sweb"	  	: ["Store Name: ", "Costco"],
+		"sLocation"	: ["Require Refrigeration ", "NO"],
+		"npurchase"	: ["Favorite: ", "No"],
+		"priority"	: ["Days that last: ", "8"],
+		"date"		: ["Date: ", "11/14/13"],
+		"notes"		: ["Product Notes: ", "Make good Juice with some sugar"]
+		
+	},
+	"store13":{
+		"category"	: ["Item Category: ", "Grapes"],
+		"fname"	  	: ["Fruit Name: ", "Seedles Grapes"],
+		"sweb"	  	: ["Store Name: ", "StopNshop"],
+		"sLocation"	: ["Require Refrigeration ", "Yes"],
+		"npurchase"	: ["Favorite: ", "NO"],
+		"priority"	: ["Days that last: ", "9"],
+		"date"		: ["Date: ", "10/1/13"],
+		"notes"		: ["Product Notes: ", "Nice for Snak"]
+		
+	},
+	"store14":{
+			"category"	: ["Item Category: ", "Tropical"],
+		"fname"	  	: ["Fruit Name: ", "Water Melon"],
+		"sweb"	  	: ["Store Name: ", "ShopRite"],
+		"sLocation"	: ["Require Refrigeration ", "Yes"],
+		"npurchase"	: ["Favorite: ", "No"],
+		"priority"	: ["Days that last: ", "4"],
+		"date"		: ["Date: ", "11/19/13"],
+		"notes"		: ["Product Notes: ", "Great for snacks and smothies"]
+		
+	},
+	"store15":{
+			"category"	: ["Item Category: ", "Other"],
+		"fname"	  	: ["Fruit Name: ", "Pears"],
+		"sweb"	  	: ["Store Name: ", "Costco"],
+		"sLocation"	: ["Require Refrigeration ", "Yes"],
+		"npurchase"	: ["Favorite: ", "No"],
+		"priority"	: ["Days that last: ", "8"],
+		"date"		: ["Date: ", "10/12/13"],
+		"notes"		: ["Product Notes: ", "Good for desert"]
+		
+	},
+	"store16":{
+			"category"	: ["Item Category: ", "Berries"],
+		"fname"	  	: ["Fruit Name: ", "Black Berry"],
+		"sweb"	  	: ["Store Name: ", "Wegmans"],
+		"sLocation"	: ["Require Refrigeration ", "Yes"],
+		"npurchase"	: ["Favorite: ", "Yes"],
+		"priority"	: ["Days that last: ", "7"],
+		"date"		: ["Date: ", "11/01/13"],
+		"notes"		: ["Product Notes: ", "Can be combine with Greek Yogurt"]
+		
+	},
+	"store17":{
+			"category"	: ["Item Category: ", "Citrus"],
+		"fname"	  	: ["Fruit Name: ", "Lime"],
+		"sweb"	  	: ["Store Name: ", "ShopRIte"],
+		"sLocation"	: ["Require Refrigeration ", "Yes"],
+		"npurchase"	: ["Favorite: ", "No"],
+		"priority"	: ["Days that last: ", "7"],
+		"date"		: ["Date: ", "11/6/13"],
+		"notes"		: ["Product Notes: ", "Great for Pie"]
+		
+	},
+	"store18":{
+			"category"	: ["Item Category: ", "Grapes"],
+		"fname"	  	: ["Fruit Name: ", "Red Table Grapes"],
+		"sweb"	  	: ["Store Name: ", "Sam's"],
+		"sLocation"	: ["Require Refrigeration ", "Yes"],
+		"npurchase"	: ["Favorite: ", "no"],
+		"priority"	: ["Days that last: ", "8"],
+		"date"		: ["Date: ", "11/16/13"],
+		"notes"		: ["Product Notes: ", "Good source of Vitamin D"]
+		
+	},
+	"store19":{
+			"category"	: ["Item Category: ", "Tropical"],
+		"fname"	  	: ["Fruit Name: ", "Papaya"],
+		"sweb"	  	: ["Store Name: ", "ShopRIte"],
+		"sLocation"	: ["Require Refrigeration ", "No"],
+		"npurchase"	: ["Favorite: ", "Yes"],
+		"priority"	: ["Days that last: ", "10"],
+		"date"		: ["Date: ", "11/12/13"],
+		"notes"		: ["Product Notes: ", "Nice for Desert and Snack"]
+		
+	},
+	"store20":{
+			"category"	: ["Item Category: ", "Other"],
+		"fname"	  	: ["Fruit Name: ", "Tomato"],
+		"sweb"	  	: ["Store Name: ", "Costco"],
+		"sLocation"	: ["Require Refrigeration ", "Yes"],
+		"npurchase"	: ["Favorite: ", "No"],
+		"priority"	: ["Days that last: ", "10"],
+		"date"		: ["Date: ", "11/20/13"],
+		"notes"		: ["Product Notes: ", "Good for snack and salads"]
+		
+	}
+	
+}
